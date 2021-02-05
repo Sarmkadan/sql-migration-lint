2526d230151d3238c27830dd2232aff68215efc9
Vladyslav Zaiets <rutova2@gmail.com>
2021-02-05 18:58:39 +0200

    tidy up AssemblyMarker

---
+++ README.md
@@ -0,0 +1,5 @@
+# sql-migration-lint
+
+Lints EF Core migrations for dangerous operations before they hit production.
+
+> v0.1 in progress.
---
+++ src/AssemblyMarker.cs
@@ -0,0 +1,4 @@
+namespace SqlMigrationLint;
+
+// Placeholder so the package compiles before feature files land.
+internal static class AssemblyMarker { }
