34642b4bf44f2b1a065382a37ca2f24f5f22e17e
Vladyslav Zaiets <rutova2@gmail.com>
2021-02-10 17:25:30 +0200

    Validate DestructiveOperationRules args
    no behaviour change intended.

---
+++ src/DestructiveOperationRulesValidation.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+
+namespace SqlMigrationLint;
+
+/// <summary>
+/// Provides validation helpers for <see cref="DestructiveOperationRules"/> static class and its members.
+/// </summary>
+public static class DestructiveOperationRulesValidation
+{
+    /// <summary>
+    /// Validates the <see cref="DestructiveOperationRules"/> static class members.
+    /// </summary>
+    /// <returns>A list of validation problems; empty if valid.</returns>
+    public static IReadOnlyList<string> Validate()
+    {
+        var problems = new List<string>();
+
+        // Validate All property
+        if (DestructiveOperationRules.All is null)
+        {
+            problems.Add("The All property must not be null.");
+        }
+        else if (DestructiveOperationRules.All.Count == 0)
+        {
+            problems.Add("The All property must contain at least one lint rule.");
+        }
+
+        // Validate each rule in All
+        foreach (var rule in DestructiveOperationRules.All ?? [])
+        {
+            if (rule is null)
+            {
+                problems.Add("The All collection must not contain null elements.");
+                continue;
+            }
+
+            if (string.IsNullOrWhiteSpace(rule.Name))
+            {
+                problems.Add($"Rule '{rule.GetType().Name}' has an invalid or empty Name.");
+            }
+
+            if (string.IsNullOrWhiteSpace(rule.Description))
+            {
+                problems.Add($"Rule '{rule.Name ?? rule.GetType().Name}' has an invalid or empty Description.");
+            }
+
+            if (rule.Severity is not (LintSeverity.Blocker or LintSeverity.Danger or LintSeverity.Warning))
+            {
+                problems.Add($"Rule '{rule.Name}' has an invalid Severity value: {rule.Severity}.");
+            }
+        }
+
+        return problems;
+    }
+
+    /// <summary>
+    /// Determines whether the <see cref="DestructiveOperationRules"/> static class is valid.
+    /// </summary>
+    /// <returns>True if valid; otherwise, false.</returns>
+    public static bool IsValid()
+    {
+        return Validate().Count == 0;
+    }
+
+    /// <summary>
+    /// Ensures that the <see cref="DestructiveOperationRules"/> static class is valid.
+    /// </summary>
+    /// <exception cref="ArgumentException">Thrown if validation fails, containing the list of problems.</exception>
+    public static void EnsureValid()
+    {
+        var problems = Validate();
+
+        if (problems.Count > 0)
+        {
+            throw new ArgumentException(
+                $"DestructiveOperationRules validation failed:{Environment.NewLine}  - {
+                    string.Join($"{Environment.NewLine}  - ", problems)
+                }",
+                nameof(DestructiveOperationRules));
+        }
+    }
+}
