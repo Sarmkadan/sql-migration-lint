f7011cd6510e7cf7488c5c3c1a0faf37236f3649
Vladyslav Zaiets <rutova2@gmail.com>
2021-02-11 11:11:59 +0200

    reshuffle GitHubAnnotationsWriter a bit

---
+++ src/GitHubAnnotationsWriter.cs
@@ -0,0 +1,60 @@
+namespace SqlMigrationLint;
+
+/// <summary>
+/// Writes lint findings in GitHub Actions workflow command format.
+/// </summary>
+public static class GitHubAnnotationsWriter
+{
+    /// <summary>
+    /// Formats a single lint finding as a GitHub Actions workflow command.
+    /// </summary>
+    /// <param name="finding">The finding to format.</param>
+    /// <returns>A GitHub Actions workflow command string.</returns>
+    public static string Format(LintFinding finding)
+    {
+        var severityPrefix = finding.Severity switch
+        {
+            LintSeverity.Blocker => "error",
+            LintSeverity.Danger => "error",
+            LintSeverity.Warning => "warning",
+            _ => "notice"
+        };
+
+        var filePart = finding.File is null ? "" : $"file={finding.File},line={finding.Line ?? 0}";
+        var message = finding.Message.Replace("\n", " ").Replace("\r", " ");
+
+        return $"::{severityPrefix} {filePart}::{message}";
+    }
+
+    /// <summary>
+    /// Writes all findings to the specified text writer.
+    /// </summary>
+    /// <param name="findings">The findings to write.</param>
+    /// <param name="writer">The text writer to write to.</param>
+    public static void WriteAll(IReadOnlyList<LintFinding> findings, TextWriter writer)
+    {
+        if (findings is null)
+        {
+            throw new ArgumentNullException(nameof(findings));
+        }
+
+        if (writer is null)
+        {
+            throw new ArgumentNullException(nameof(writer));
+        }
+
+        foreach (var finding in findings)
+        {
+            writer.WriteLine(Format(finding));
+        }
+    }
+
+    /// <summary>
+    /// Writes all findings to the console output.
+    /// </summary>
+    /// <param name="findings">The findings to write.</param>
+    public static void WriteToConsole(IReadOnlyList<LintFinding> findings)
+    {
+        WriteAll(findings, Console.Out);
+    }
+}
